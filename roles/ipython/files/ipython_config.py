17cff3790823370c1bbacac2cca92c795f4717ca
David Resnick <dresnick@salesforce.com>
2020-01-04 10:52:16 +0200

    Use vi mode in ipython

--- roles/ipython/files/ipython_config.py
+++ roles/ipython/files/ipython_config.py
@@ -296,7 +296,7 @@ c.InteractiveShellApp.extensions = ["autoreload"]
 #c.TerminalInteractiveShell.display_completions = 'multicolumn'
 
 ## Shortcut style to use at the prompt. 'vi' or 'emacs'.
-c.TerminalInteractiveShell.editing_mode = 'emacs'
+c.TerminalInteractiveShell.editing_mode = 'vi'
 
 ## Set the editor used by IPython (default to $EDITOR/vi/notepad).
 #c.TerminalInteractiveShell.editor = u'vim'
